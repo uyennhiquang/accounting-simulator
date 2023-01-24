cdcff89accdad61c1e5656a48bcfd281a5474a26
Uyen Nhi Quang <111651301+uyennhiquang@users.noreply.github.com>
2023-01-23 19:38:39 -0500

    Create createNewEntry function and make changes Create a seperate function that generates a row -- it takes in 3 parameters: an array, # of iterations to loop, and whether to use elements of that array as cell's data or not. Create a createNewEntry function that generates an entry above the Add Entry Row.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const createJournalBtn = document.querySelector(".create-journal");
 const journalHeaderText = [
   "DATE",
@@ -7,11 +9,44 @@ const journalHeaderText = [
   "DEBIT",
   "CREDIT",
 ];
+
+// Generate table objects
+const table = document.createElement("table");
+
+// Function that generates a row taking an array and boolean value as inputs. It will return a row of cell of the given length and can include or not include the name of the elements inside the array.
+const generateJournalSectionRow = function (array, iteration, textInclude) {
+  const row = document.createElement("tr");
+  for (let i = 0; i < iteration; i++) {
+    // Within each iteration, a cell is generated, then the cell text
+    const cell = document.createElement("td");
+    const cellText = textInclude
+      ? document.createTextNode(array[i])
+      : document.createTextNode("Pizza");
+    // Lastly, the cell text is put within the cell tags (td) and the cell is put within the row (tr)
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+    if (textInclude && array[i] === "DATE") cell.setAttribute("colspan", `2`);
+    else if (!textInclude) cell.setAttribute("class", "entry");
+  }
+  return row;
+};
+
+const createNewEntry = function () {
+  // Generate 1 entry (2 rows) above "Add Entry" row
+  for (let i = 0; i < 2; i++) {
+    const entryRow = generateJournalSectionRow(
+      journalHeaderText,
+      journalHeaderText.length + 1,
+      false
+    );
+
+    table.appendChild(entryRow);
+    table.insertBefore(entryRow, document.getElementById("add-entry-row"));
+  }
+};
+
 createJournalBtn.addEventListener("click", function () {
   // createJournalBtn.classList.add("hidden");
-
-  // Generate table objects
-  const table = document.createElement("table");
 
   // Generate caption
   const tableCaption = document.createElement("caption");
@@ -27,35 +62,29 @@ createJournalBtn.addEventListener("click", function () {
   const clickableAddEntry = document.createElement("a");
   const textAddEntry = document.createTextNode("Add new entry");
   clickableAddEntry.href = "#";
-  clickableAddEntry.setAttribute("onclick", "newEntry(); return false;");
+  clickableAddEntry.setAttribute("onclick", "createNewEntry(); return false;");
   clickableAddEntry.setAttribute("id", "add-entry-click");
   clickableAddEntry.appendChild(textAddEntry);
 
   const cellAddEntry = document.createElement("td");
   cellAddEntry.appendChild(clickableAddEntry);
   rowAddEntry.appendChild(cellAddEntry);
-  cellAddEntry.setAttribute("colspan", journalHeaderText.length);
+  cellAddEntry.setAttribute("colspan", journalHeaderText.length + 1); // length + 1 due to DATE spanning 2 columns
+
   cellAddEntry.setAttribute("id", "add-entry-cell");
+  rowAddEntry.setAttribute("id", "add-entry-row");
 
   // Generate header elements and row to insert cells into
   const tableHeader = document.createElement("thead");
-  const rowHeader = document.createElement("tr");
+  const rowHeader = generateJournalSectionRow(
+    journalHeaderText,
+    journalHeaderText.length,
+    true
+  );
 
-  // Generate header cells
-  for (const headerText of journalHeaderText) {
-    // Within each iteration, a cell is generated, then the cell text
-    const cell = document.createElement("td");
-    const cellText = document.createTextNode(headerText);
-
-    // Lastly, the cell text is put within the cell tags (td) and the cell is put within the row (tr)
-    cell.appendChild(cellText);
-    rowHeader.appendChild(cell);
-  }
   tableHeader.appendChild(rowHeader);
   table.appendChild(tableHeader);
   table.appendChild(rowAddEntry);
-  // document.body.appendChild(table);
 
   document.body.insertBefore(table, createJournalBtn);
-  // table.setAttribute("border", "1");
 });
